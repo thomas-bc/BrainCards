06152174a2a9541dce6f97d42b927df3afffc56a
Elegy1 <thomas.boyerchammard@gmail.com>
2019-06-21 14:57:38 +0200

    Initial commit

--- config.php
+++ config.php
@@ -3,4 +3,4 @@
 $BDD_host="localhost";
 $BDD_user="root";
 $BDD_password=""; // vide sous windows
-$BDD_base="web_electif"; // nom de la base de données
+$BDD_base="braincards"; // nom de la base de données
