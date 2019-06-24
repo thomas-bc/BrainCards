9a0997b722f896e20fc14b775b36e7234926d8f7
MaximilienGr <maximilien.grattepanche@hotmail.fr>
2019-06-24 18:48:50 +0200

    chat laid mais foncitonnel

--- templates/chat.php
+++ templates/chat.php
@@ -6,7 +6,39 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
     die("");
 }
 
+include_once("libs/modele.php");
+include_once("libs/maLibUtils.php");
+
+// On récupère l'id de la conversation à afficher, dans idConv
+//$idBrainsto = getValue("BR_id");
+
+$idBrainsto=1;
+if (!$idBrainsto)
+{
+    die("idBrainsto manquant");
+}
+
+// Les messages
+$messages = getMessages($idBrainsto);
+
+foreach($messages as $dataMessage) {
+    echo '<div>';
+    echo "[" . $dataMessage["msg_auteur_id"] . "] " ;
+    echo $dataMessage["msg_contenu"];
+    echo "</div>\n";
+}
+
 ?>
+
+
+<script>
+    // DANS 10 SECONDES, Raffraichir la page
+        window.setTimeout(fnReload,10000);
+
+        function fnReload(){
+            document.location.reload();
+        }
+</script>
 
 
 <h1>Chat</h1>
