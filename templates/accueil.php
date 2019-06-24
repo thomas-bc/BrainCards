6481d2cac56cbced9653d8ee5713f6c65da218fc
MaximilienGr <maximilien.grattepanche@hotmail.fr>
2019-06-24 16:48:58 +0200

    formLogin page d'acceuil

--- templates/accueil.php
+++ templates/accueil.php
@@ -11,3 +11,16 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 <h1>Accueil</h1>
 
+<p>BrainCards est un site de Brainstorming à Distance. Il permet à l’organisateur du Brainstorming (le
+    « Master » ) de faire respecter les différents temps du Brainstorming. Il permet également d’afficher les
+    traditionnels « Post-it » sous une forme différente, plus complète et plus à même d’alimenter
+    l’imagination des différents participant.</p>
+
+
+<div id="formLogin">
+    <form action="controleur.php" method="GET">
+        Login : <input type="text" name="login" /><br />
+        Passe : <input type="password" name="passe" /><br />
+        <input type="submit" name="action" value="Connexion" />
+    </form>
+</div>
