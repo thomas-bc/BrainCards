614997f5e7dc810a7e96b71effdd2799b4760d36
MaximilienGr <maximilien.grattepanche@hotmail.fr>
2019-06-24 16:57:16 +0200

    join

--- templates/join.php
+++ templates/join.php
@@ -11,3 +11,17 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 <h1>Join</h1>
 
+<p>BrainCards est un site de Brainstorming à Distance. Il permet à l’organisateur du Brainstorming (le
+    « Master » ) de faire respecter les différents temps du Brainstorming. Il permet également d’afficher les
+    traditionnels « Post-it » sous une forme différente, plus complète et plus à même d’alimenter
+    l’imagination des différents participant.</p>
+
+
+<div id="formLoginJoin">
+    <form action="controleur.php" method="GET">
+        Login : <input type="text" name="login" /><br />
+        Passe : <input type="password" name="passe" /><br />
+        Passe à nouveau : <input type="password" name="passe2" /><br />
+        <input type="submit" name="action" value="Créer un compte" />
+    </form>
+</div>
