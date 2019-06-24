92682a1f5ea23aeab5838bf0db4fe187701a28a4
MaximilienGr <maximilien.grattepanche@hotmail.fr>
2019-06-24 17:04:29 +0200

    deplacement de join dans inscription

---
+++ templates/inscription.php
@@ -0,0 +1,28 @@
+<?php
+// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+if (basename($_SERVER["PHP_SELF"]) != "index.php")
+{
+    header("Location:../index.php?view=join");
+    die("");
+}
+
+?>
+
+<p>BrainCards est un site de Brainstorming à Distance. Il permet à l’organisateur du Brainstorming (le
+    « Master » ) de faire respecter les différents temps du Brainstorming. Il permet également d’afficher les
+    traditionnels « Post-it » sous une forme différente, plus complète et plus à même d’alimenter
+    l’imagination des différents participant.</p>
+
+
+<div id="formLoginJoin">
+    <form action="controleur.php" method="GET">
+    Login : <input type="text" name="login" /><br />
+        Passe : <input type="password" name="passe" /><br />
+        Passe à nouveau : <input type="password" name="passe2" /><br />
+        <input type="submit" name="action" value="Créer un compte" />
+    </form>
+</div>
+
+<p id="mauvaiseCorrespondance">Les deux mots de passe ne correspondent pas</p>
+<p id="pseudoDejaExistant">Le pseudo est déjà pris</p>
+<p id="reussi">Votre comtpe a bien été crée ! Vous pouvez désormais vous connecter.</p>
--- templates/join.php
+++ templates/join.php
@@ -10,22 +10,3 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 
 <h1>Join</h1>
-
-<p>BrainCards est un site de Brainstorming à Distance. Il permet à l’organisateur du Brainstorming (le
-    « Master » ) de faire respecter les différents temps du Brainstorming. Il permet également d’afficher les
-    traditionnels « Post-it » sous une forme différente, plus complète et plus à même d’alimenter
-    l’imagination des différents participant.</p>
-
-
-<div id="formLoginJoin">
-    <form action="controleur.php" method="GET">
-        Login : <input type="text" name="login" /><br />
-        Passe : <input type="password" name="passe" /><br />
-        Passe à nouveau : <input type="password" name="passe2" /><br />
-        <input type="submit" name="action" value="Créer un compte" />
-    </form>
-</div>
-
-<p id="mauvaiseCorrespondance">Les deux mots de passe ne correspondent pas</p>
-<p id="pseudoDejaExistant">Le pseudo est déjà pris</p>
-<p id="reussi">Votre comtpe a bien été crée ! Vous pouvez désormais vous connecter.</p>
