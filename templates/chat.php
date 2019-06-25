2e49f48b9512f1b06347ce8815439225aa765fb7
Pierrick Gervasi <Pierrick Gervasi@gmail.com>
2019-06-25 22:46:46 +0200

    Solution Chat qui marche mais affiche les messages au mauvais endroit un bref instant avant de les append ailleurs.
    Je vais essayer de créer une variable qu'on ajoute pas

--- templates/chat.php
+++ templates/chat.php
@@ -20,23 +20,82 @@ if (!$idBrainsto)
 // Les messages
 $messages = getMessages($idBrainsto);
 
+echo '<div id="recupChat">';
+
 foreach($messages as $dataMessage) {
-    echo '<div>';
+    echo '<li>';
     echo "[" . $dataMessage["msg_auteur_id"] . "] " ;
     echo $dataMessage["msg_contenu"];
-    echo "</div>\n";
+    echo "</li>\n";
 }
-echo '<form action="controleur.php" method="GET" >';
 
-echo '<input type="text" name="message" >';
-echo '<input type="hidden" name="idBrainsto" value="$idBrainsto" > ';
-echo '<input type="submit" name="action" value="Poster" >';
-
-echo '</form>';
+echo '</div>';
 ?>
+
+<link rel="stylesheet" href="css/cssCommun.css">
+
+<style>
+
+    h2{
+        color:#ED7D31;
+        margin-top:20px;
+        text-align:center;
+    }
+
+    li{
+        color:#ED7D31;
+    }
+
+    #chat{
+        position:absolute;
+        background-color: white ;
+        width:300px;
+        right:0;
+        top:0;
+        bottom:0;
+        color:#ED7D31;
+    }
+
+    form{
+        position:absolute;
+        bottom:10px;
+        right:40px;
+    }
+
+
+    .button {
+        color: #ED7D31;
+    }
+
+    .textInput{
+        border-bottom:1px solid #ED7D31;
+        color:#ED7D31;
+    }
+
+    #recupChat li{
+        display:block;
+        margin-left:15px;
+    }
+
+</style>
+
+<script src="js/jquery-3.4.1.js"></script>
 
 
 <script>
+
+    $(document).ready(function(){
+
+        $("#affichageChat").append($("#recupChat"));
+
+
+    })
+
+
+
+
+
+
     // DANS 10 SECONDES, Raffraichir la page
         window.setTimeout(fnReload,10000);
 
@@ -46,4 +105,23 @@ echo '</form>';
 </script>
 
 
-<h1>Chat</h1>
+
+
+
+
+<div id="chat">
+    <h2>Chat</h2>
+
+    <div id="affichageChat"></div>
+
+    <form action="controleur.php" method="GET" >
+
+        <input class="textInput" type="text" name="message" >
+        <input type="hidden" name="idBrainsto" value="$idBrainsto" >
+        <input class="button" type="submit" name="action" value="Poster" >
+
+        </form>
+
+
+</div>
+
