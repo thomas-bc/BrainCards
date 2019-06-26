4f9417558e50ca22b0fdf175f8b5334a14008cd2
NathanBoisard <48566109+NathanBoisard@users.noreply.github.com>
2019-06-26 16:58:45 +0200

    create dataProvider

---
+++ dataProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("libs/maLibUtils.php");
+include_once("libs/maLibSQL.pdo.php");
+include_once("libs/modele.php");
+
+
+
+// faire des if pour chaque requete AJAX
+if ($ma_valeur_associe_a_mon_mon_cle = valider("mon_mot_cle")) { // si mon mot clé existe
+
+    //j'utilise $ma_valeur_associe_a_mon_mon_cle
+
+    // et je fais des bails genre :
+
+    /*
+    $SQL = "SELECT * FROM etudiants WHERE ";
+    $users = parcoursRs(SQLSelect($SQL));
+    $data["students"] = $users;
+
+    $data["cle"] = "valeur";
+
+    echo json_encode($data);
+    */
+    // on envoie des données au format JSON
+}
